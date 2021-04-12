d839a3b3f78e05c2c89d5a0696279fc67fd1f507
Mori <moriakaice@gmail.com>
2021-04-13 00:40:05 +0200

    Don't hack home

--- src/browserAutoHack.js
+++ src/browserAutoHack.js
@@ -1,7 +1,11 @@
 clearInterval(autoHack)
 
 let autoHack = setInterval(() => {
-  if (document.querySelector('#terminal-input-text-box')) {
+  if (
+    document.querySelector('#terminal-input-text-box') &&
+    document.querySelector('#terminal-input-header') &&
+    !document.querySelector('#terminal-input-header').textContent.includes('home')
+  ) {
     document.querySelector('#terminal-input-text-box').value = 'hack'
     const ke = new KeyboardEvent('keydown', {
       bubbles: true,

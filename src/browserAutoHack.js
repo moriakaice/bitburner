edb4ddea4223b0993998333c2f4c89d935bd154b
Mori <moriakaice@gmail.com>
2021-04-12 08:06:27 +0200

    AutoHack to ease BN-8

---
+++ src/browserAutoHack.js
@@ -0,0 +1,13 @@
+clearInterval(autoHack)
+
+let autoHack = setInterval(() => {
+  if (document.querySelector('#terminal-input-text-box')) {
+    document.querySelector('#terminal-input-text-box').value = 'hack'
+    const ke = new KeyboardEvent('keydown', {
+      bubbles: true,
+      cancelable: true,
+      keyCode: 13,
+    })
+    document.body.dispatchEvent(ke)
+  }
+}, 100)

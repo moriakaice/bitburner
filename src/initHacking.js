3fc824fd7111c30c5e89b2186133edfa3f31ac1e
Alexander Saynisch <alexander@saynisch.de>
2022-02-04 13:28:00 +0100

    revert repo adresse for pr

--- src/initHacking.js
+++ src/initHacking.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://raw.githubusercontent.com/ComictypX/bitburner/master/src/'
+const baseUrl = 'https://raw.githubusercontent.com/moriakaice/bitburner/master/src/'
 const filesToDownload = [
   'common.js',
   'mainHack.js',

c35f78c22297da0db321d973f8e9c6d4c8b8b168
Saynt-Garmo <92912875+Saynt-Garmo@users.noreply.github.com>
2022-07-27 04:53:46 -0400

    Update and rename stockMarketer4S.ns to stockMarketer4S.js

--- src/stockMarketer4S.js
+++ src/stockMarketer4S.js
@@ -17,7 +17,7 @@ function getMoney(ns) {
 
 function sellShorts(ns, stockSymbol) {
   const stockInfo = getStockInfo(ns, stockSymbol)
-  const shortSellValue = ns.sellShort(stockSymbol, stockInfo.sharesShort)
+  const shortSellValue = ns.stock.sellShort(stockSymbol, stockInfo.sharesShort)
 
   if (shortSellValue) {
     corpus += stockInfo.sharesShort * (stockInfo.avgPriceShort - shortSellValue) - 2 * commission
@@ -32,7 +32,7 @@ function sellShorts(ns, stockSymbol) {
 
 function sellLongs(ns, stockSymbol) {
   const stockInfo = getStockInfo(ns, stockSymbol)
-  const longSellValue = ns.sellStock(stockSymbol, stockInfo.sharesLong)
+  const longSellValue = ns.stock.sellStock(stockSymbol, stockInfo.sharesLong)
 
   if (longSellValue) {
     corpus += stockInfo.sharesLong * (longSellValue - stockInfo.avgPriceLong) - 2 * commission
@@ -86,14 +86,14 @@ function buyNewShares(ns, stockSymbol) {
       maxSharesToBuy = stockInfo.maxShares - stockInfo.sharesLong
       sharesToBuy = Math.max(0, Math.min(maxSharesToBuy, Math.floor(getMoney(ns) / stockInfo.stockAskPrice)))
       if (sharesToBuy) {
-        buyValue = ns.buyStock(stockSymbol, sharesToBuy)
+        buyValue = ns.stock.buyStock(stockSymbol, sharesToBuy)
       }
       shareType = 'longs'
     } else {
       maxSharesToBuy = stockInfo.maxShares - stockInfo.sharesShort
       sharesToBuy = Math.max(0, Math.min(maxSharesToBuy, Math.floor(getMoney(ns) / stockInfo.stockBidPrice)))
       if (sharesToBuy) {
-        buyValue = ns.shortStock(stockSymbol, sharesToBuy)
+        buyValue = ns.stock.short(stockSymbol, sharesToBuy)
       }
       shareType = 'shorts'
     }
@@ -106,17 +106,17 @@ function buyNewShares(ns, stockSymbol) {
 }
 
 function getStockInfo(ns, stockSymbol) {
-  const [sharesLong, avgPriceLong, sharesShort, avgPriceShort] = ns.getStockPosition(stockSymbol)
-  const volatility = ns.getStockVolatility(stockSymbol)
-  const probability = ns.getStockForecast(stockSymbol) - 0.5
+  const [sharesLong, avgPriceLong, sharesShort, avgPriceShort] = ns.stock.getPosition(stockSymbol)
+  const volatility = ns.stock.getVolatility(stockSymbol)
+  const probability = ns.stock.getForecast(stockSymbol) - 0.5
   const expectedReturn = Math.abs(volatility * probability)
-  const maxShares = ns.getStockMaxShares(stockSymbol)
+  const maxShares = ns.stock.getMaxShares(stockSymbol)
 
   const haveAnyShares = sharesLong + sharesShort > 0
   const haveMaxShares = sharesLong + sharesShort === maxShares
 
-  const stockAskPrice = ns.getStockAskPrice(stockSymbol)
-  const stockBidPrice = ns.getStockBidPrice(stockSymbol)
+  const stockAskPrice = ns.stock.getAskPrice(stockSymbol)
+  const stockBidPrice = ns.stock.getBidPrice(stockSymbol)
 
   const position = probability >= 0 ? 'Long' : 'Short'
 
@@ -142,7 +142,7 @@ export async function main(ns) {
   ns.disableLog('ALL')
   let tickCounter = 1
 
-  stockSymbols = ns.getStockSymbols()
+  stockSymbols = ns.stock.getSymbols()
 
   corpus = ns.getServerMoneyAvailable('home') - 1000000
   stockSymbols.forEach((stockSymbol) => {
